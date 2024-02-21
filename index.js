efe95dccc447de6ab0957a59dcc46f05c7eab242
Sanvika Kumar <sanvika890@gmail.com>
2024-02-21 17:17:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
