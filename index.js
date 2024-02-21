fef3bacc4da55deab9590ae3b8b5a72ef7286803
Sanvika Kumar <sanvika890@gmail.com>
2024-02-21 17:48:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ var app = express();
 
 app.use(cors());
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
